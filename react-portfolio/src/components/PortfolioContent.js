0cb6c25a00dc73c24b032d505527b3162cfe9429
Simona <simona.snapk@gmail.com>
2022-07-20 18:42:25 -0700

    adding functionality to navtabs and content

---
+++ react-portfolio/src/components/PortfolioContent.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import AboutMe from "./pages/About";
+import ContactInfo from "./pages/Contact";
+
+export default function PortfolioContent()
+{
+    const [currentPage, setCurrentPage] = useState("Home");
+
+    const getPage = () =>
+    {
+        if (currentPage === "Home")
+        {
+            return <Home />;
+        }
+        if (currentPage === "About")
+        {
+            return <AboutMe />;
+        }
+        if (currentPage === "Contact")
+        {
+            return <ContactInfo />
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+
+
+}
